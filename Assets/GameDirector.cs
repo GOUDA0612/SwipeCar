360f925db30481bcf0ea42f17bb8ea42320c9fa7
takamino <minorutakanott@gmail.com>
2025-04-03 14:05:32 +0900

    UI追加

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+   public Transform car;
+   public Transform flag;
+   public TextMeshProUGUI distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length=this.flag.position.x - this.car.position.x;
+        this.distance.text="Distance:"+length.ToString("F2")+"m";
+        
+    }
+}
